495fd3c982003797a99fffa2c1fca03757cb787a
logchan <loliconlg@gmail.com>
2016-11-16 15:54:11 +0800

    Add navbar page loading

---
+++ src/RmBackend/wwwroot/js/rm-navbar.js
@@ -0,0 +1,17 @@
+﻿function loadNavbar() {
+    $.get(apiserver + '/api/page/navbar',
+        function (data) {
+            var ul = $('#navbar-ul');
+            data.sort(function (a, b) { return a.NavbarOrder - b.NavbarOrder });
+
+            for (var i in data) {
+                if (data.hasOwnProperty(i)) {
+                    var page = data[i];
+                    var li = '<li><a href="/page/' + page.Path + '">' + page.Title + '</a></li>';
+                    ul.append(li);
+                }
+            }
+        });
+}
+
+loadNavbar();
